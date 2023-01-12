83c67d4122a7a0a86f87f5d28423f13d8101ab5a
Pushpendra Singh Thakur <106228336+softech123@users.noreply.github.com>
2023-01-12 15:07:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,124 +2,184 @@ const commentWrapper = document.querySelector(".comment-wrapper");
 const commentInput = document.querySelector(".comment-input");
 const addButton = document.querySelector(".add-button");
 
-const comments = [
-    //   { id: 1, text: "This is a comment", likes: 0, replies: [] },
-];
+const localStorageKey = "state";
+
+const initializeComments = () => {
+  const state = localStorage.getItem(localStorageKey);
+
+  if (!state) {
+    return [];
+  }
+
+  return JSON.parse(state);
+};
+
+const comments = initializeComments();
+
+const saveState = () => {
+  const state = JSON.stringify(comments);
+  localStorage.setItem(localStorageKey, state);
+};
 
 const createCommentObject = (commentText) => {
-    return {
-        id: Math.random(),
-        text: commentText,
-        likes: 0,
-        replies: [],
-    };
+  return {
+    id: Math.random(),
+    text: commentText,
+    likes: 0,
+    replies: [],
+  };
+};
+
+const findCommentObject = (comments, commentId) => {
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
+
+    if (comment.id === commentId) {
+      return comment;
+    }
+
+    const foundComment = findCommentObject(comment.replies, commentId);
+
+    if (foundComment) {
+      return foundComment;
+    }
+  }
+};
+
+const deleteComment = (comments, commentId) => {
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
+
+    if (comment.id === commentId) {
+      comments.splice(i, 1);
+      return;
+    }
+
+    deleteComment(comment.replies, commentId);
+  }
 };
 
 const createCommentNode = (comment) => {
-    const commentNode = document.createElement("div");
-    commentNode.classList.add("comment", "hide-reply");
+  const commentNode = document.createElement("div");
+  commentNode.classList.add("comment", "hide-reply");
 
-    const commentText = document.createElement("div");
-    commentText.classList.add("comment-text");
-    commentText.innerText = comment.text;
+  const commentText = document.createElement("div");
+  commentText.classList.add("comment-text");
+  commentText.innerText = comment.text;
 
-    const buttonsAndLikesWrapper = document.createElement("div");
-    buttonsAndLikesWrapper.classList.add("button-and-likes-wrapper");
+  const buttonsAndLikesWrapper = document.createElement("div");
+  buttonsAndLikesWrapper.classList.add("button-and-likes-wrapper");
 
-    const replyButton = document.createElement("button");
-    replyButton.classList.add("button", "success");
-    replyButton.innerText = "Reply";
-    replyButton.onclick = () => commentNode.classList.toggle("hide-reply");
+  const replyButton = document.createElement("button");
+  replyButton.classList.add("button", "success");
+  replyButton.innerText = "Reply";
+  replyButton.onclick = () => commentNode.classList.toggle("hide-reply");
 
-    const likeButton = document.createElement("button");
-    likeButton.classList.add("button", "success");
-    likeButton.innerText = "Like";
+  const likeButton = document.createElement("button");
+  likeButton.classList.add("button", "success");
+  likeButton.innerText = "Like";
+  likeButton.onclick = () => {
+    comment.likes++;
+    saveState();
+    renderComments();
+  };
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("button", "delete");
-    deleteButton.innerText = "Delete";
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("button", "delete");
+  deleteButton.innerText = "Delete";
+  deleteButton.onclick = () => {
+    deleteComment(comments, comment.id);
+    saveState();
+    renderComments();
+  };
 
-    const likeText = document.createElement("div");
-    likeText.innerText = `${comment.likes} likes`;
-    likeText.classList.add("likes-text");
+  const likeText = document.createElement("div");
+  likeText.innerText = `${comment.likes} likes`;
+  likeText.classList.add("likes-text");
 
-    const replyWrapper = document.createElement("div");
-    replyWrapper.classList.add("reply-wrapper");
+  const replyWrapper = document.createElement("div");
+  replyWrapper.classList.add("reply-wrapper");
 
-    const replyInput = document.createElement("input");
+  const replyInput = document.createElement("input");
 
-    const addReplyButton = document.createElement("button");
-    addReplyButton.classList.add("button", "success");
-    addReplyButton.innerText = "Add";
-    addReplyButton.onclick = () => {
-        const replyText = replyInput.value;
-        const commentId = comment.id;
+  const addReplyButton = document.createElement("button");
+  addReplyButton.classList.add("button", "success");
+  addReplyButton.innerText = "Add";
+  addReplyButton.onclick = () => {
+    const replyText = replyInput.value;
+    const commentId = comment.id;
 
-        const newReplyObject = createCommentObject(replyText);
+    if (replyText === "") {
+      alert("Please enter a reply");
+      return;
+    }
 
-        const commentObj = comments.find(
-            (commentObject) => commentObject.id === commentId
-        );
+    const newReplyObject = createCommentObject(replyText);
 
-        commentObj.replies.push(newReplyObject);
+    const commentObj = findCommentObject(comments, commentId);
 
-        renderComments();
-    };
+    commentObj.replies.push(newReplyObject);
 
-    const cancelReplyButton = document.createElement("button");
-    cancelReplyButton.classList.add("button", "delete");
-    cancelReplyButton.innerText = "Cancel";
-    cancelReplyButton.onclick = () => commentNode.classList.add("hide-reply");
+    saveState();
+    renderComments();
+  };
 
-    const replyCommentsDomArray = comment.replies.map((reply) => {
-        return createCommentNode(reply);
-    });
+  const cancelReplyButton = document.createElement("button");
+  cancelReplyButton.classList.add("button", "delete");
+  cancelReplyButton.innerText = "Cancel";
+  cancelReplyButton.onclick = () => commentNode.classList.add("hide-reply");
 
-    commentNode.appendChild(commentText);
-    commentNode.appendChild(buttonsAndLikesWrapper);
-    commentNode.appendChild(replyWrapper);
+  const replyCommentsDomArray = comment.replies.map((reply) => {
+    return createCommentNode(reply);
+  });
 
-    buttonsAndLikesWrapper.appendChild(replyButton);
-    buttonsAndLikesWrapper.appendChild(likeButton);
-    buttonsAndLikesWrapper.appendChild(deleteButton);
-    buttonsAndLikesWrapper.appendChild(likeText);
+  commentNode.appendChild(commentText);
+  commentNode.appendChild(buttonsAndLikesWrapper);
+  commentNode.appendChild(replyWrapper);
 
-    replyWrapper.appendChild(replyInput);
-    replyWrapper.appendChild(addReplyButton);
-    replyWrapper.appendChild(cancelReplyButton);
+  buttonsAndLikesWrapper.appendChild(replyButton);
+  buttonsAndLikesWrapper.appendChild(likeButton);
+  buttonsAndLikesWrapper.appendChild(deleteButton);
+  buttonsAndLikesWrapper.appendChild(likeText);
 
-    replyCommentsDomArray.forEach((replyDom) => {
-        commentNode.appendChild(replyDom);
-    });
+  replyWrapper.appendChild(replyInput);
+  replyWrapper.appendChild(addReplyButton);
+  replyWrapper.appendChild(cancelReplyButton);
 
-    return commentNode;
+  replyCommentsDomArray.forEach((replyDom) => {
+    commentNode.appendChild(replyDom);
+  });
+
+  return commentNode;
 };
 
 const renderComments = () => {
-    console.log(comments);
-    commentWrapper.innerText = "";
+  commentWrapper.innerText = "";
 
-    comments.forEach((comment) => {
-        const commentNode = createCommentNode(comment);
+  comments.forEach((comment) => {
+    const commentNode = createCommentNode(comment);
 
-        commentWrapper.appendChild(commentNode);
-    });
+    commentWrapper.appendChild(commentNode);
+  });
 };
 
 const addComment = () => {
-    const commentText = commentInput.value;
+  const commentText = commentInput.value;
 
-    if (commentText === "") {
-        alert("Please enter a comment");
-        return;
-    }
+  if (commentText === "") {
+    alert("Please enter a comment");
+    return;
+  }
 
-    const newComment = createCommentObject(commentText);
+  const newCommentObject = createCommentObject(commentText);
 
-    comments.push(newComment);
-    commentInput.value = "";
+  comments.push(newCommentObject);
+  saveState();
 
-    renderComments();
+  commentInput.value = "";
+
+  renderComments();
 };
 
 addButton.addEventListener("click", addComment);
+renderComments();
